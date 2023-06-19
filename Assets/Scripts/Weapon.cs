ce6f33f3c468dd549d139dd10bd3e33e678a2702
Harshit <harshit1507@yahoo.com>
2023-06-19 19:11:00 +0530

    deal enemy damage

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Search;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Weapon : MonoBehaviour
 {
     [SerializeField] private Camera FPCamera;
     [SerializeField] private float range = 100f;
+    [SerializeField] private float damage = 10;
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -18,7 +20,20 @@ public class Weapon : MonoBehaviour
     void Shoot()
     {
         RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-        Debug.Log("I hit this thing: " + hit.transform.name);
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            //Debug.Log("I hit this thing: " + hit.transform.name);
+            //TODO: add some hit effect for visual effects
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (!target)
+                return;
+            
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
+        
     }
 }
