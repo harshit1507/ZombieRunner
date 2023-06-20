c71c28cfdf025c6f9086b76e289578b135d61dc7
Harshit <harshit1507@yahoo.com>
2023-06-20 16:51:02 +0530

    weapon zoom sensitivity

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using StarterAssets;
 using UnityEngine;
 
 public class WeaponZoom : MonoBehaviour
@@ -9,12 +10,15 @@ public class WeaponZoom : MonoBehaviour
     [SerializeField] private float zoomedOutFOV = 60;
     [SerializeField] private float zoomedInFOV = 20;
     [SerializeField] private Animator animator;
+    [SerializeField] private float ZoomOutSensitivity =1f;
+    [SerializeField] private float ZoomInSensitivity = 0.25f;
 
+    private FirstPersonController fpsController;
     private bool zoomedInToggle = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fpsController = GetComponent<FirstPersonController>();
     }
 
     // Update is called once per frame
@@ -25,12 +29,14 @@ public class WeaponZoom : MonoBehaviour
             if (!zoomedInToggle)
             {
                 zoomedInToggle = true;
+                fpsController.RotationSpeed = ZoomInSensitivity;
                 //FPSCamera.m_Lens.FieldOfView = zoomedInFOV;
                 animator.SetBool("isZoomed", true);
             }
             else
             {
                 zoomedInToggle = false;
+                fpsController.RotationSpeed = ZoomOutSensitivity;
                 //FPSCamera.m_Lens.FieldOfView = zoomedOutFOV;
                 animator.SetBool("isZoomed", false);
             }
